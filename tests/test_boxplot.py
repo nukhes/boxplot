a610090ca4698d413832555a817fdfc422bb1f8c
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-05-23 06:26:58 -0300

    create quartis tests

--- tests/test_boxplot.py
+++ tests/test_boxplot.py
@@ -2,14 +2,14 @@ from boxplot import *
 
 x = [1,2,3,4,5,6,7,8,9]
 y = [1,2,3,4,5,6,7,8]
-z = []
+z = [1,5]
 
 def test_mediana():
     assert boxplot.mediana(x) == 5
     assert boxplot.mediana(y) == 4.5
-    assert boxplot.mediana(z) == 0
+    assert boxplot.mediana(z) == 3
 
-# def test_quartis():
-#     assert boxplot.quartis(x) == 0
-#     assert boxplot.quartis(y) == 0
-#     assert boxplot.quartis(z) == 0
+def test_quartis():
+    assert boxplot.quartis(x, raw=True) == "min: 1.00, q1: 2.50, q2: 5.00, q3: 7.50, max: 9.00"
+    assert boxplot.quartis(y, raw=True) == "min: 1.00, q1: 2.50, q2: 4.50, q3: 6.50, max: 8.00"
+    assert boxplot.quartis(z, raw=True) == "min: 1.00, q1: 1.00, q2: 3.00, q3: 5.00, max: 5.00"
